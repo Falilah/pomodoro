893a0776990111b567ec5f8ebb0331405778044f
faliah <leogoldf21@gmail.com>
2024-09-10 13:32:46 +0100

    updated

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use clap::Parser;
+use anyhow::{Context, Result};
+
+#[derive(Parser)]
+struct Timer{
+    /// how many minutes of work
+    work_minutes: u64,
+    
+    /// how many minutes of break after work
+    break_minutes: u64,
+
+}
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let args = Timer::parse();
+    println!("{}, {}",args.work_minutes, args.break_minutes)
+
+
 }
